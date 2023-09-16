27992661861351ac9eca0eb950cd12a0d07a4a38
jikor1st <jikor1st@naver.com>
2023-09-17 01:08:23 +0900

    feat: prettier 및 workspace vscode 설정 세팅한다

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	function prettierTest() {
+		console.log('em');
+	}
+	return (
+		<Html lang='en'>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
