b2d8ee3691958ff12547ff68a53996ab08b1bd41
jikor1st <jikor1st@naver.com>
2023-09-17 16:55:03 +0900

    feat: eslint 초기 설정을 진행한다.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-	function prettierTest() {
-		console.log('em');
-	}
 	return (
 		<Html lang='en'>
 			<Head />
