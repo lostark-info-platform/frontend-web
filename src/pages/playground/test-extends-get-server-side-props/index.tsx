261157f39038b8fe766d9b7c29589f7ae7ee83d7
jikor1st <jikor1st@naver.com>
2023-10-25 00:08:05 +0900

    feat: serverSideProps를 확장하는 함수와 serverSideError class를 생성한다

--- src/pages/playground/test-extends-get-server-side-props/index.tsx
+++ src/pages/playground/test-extends-get-server-side-props/index.tsx
@@ -1,9 +1,35 @@
+import useTestReactQuery from '@/hooks/testReactQuery/useTestReactQuery';
+import api from '@/services/api';
+import extendsGetServerSideProps from '@/services/next/extendsGetServerSideProps';
+// import ServerSideError from '@/services/next/serverSideError';
+import testReactQueryKeys from '@/services/queryKeys/testReactQuery';
+
 const TestExtendsGetServerSidePropsPage = () => {
-	return <></>;
+	const { data } = useTestReactQuery({ id: '2' });
+	return <>{data?.testReactQuery}</>;
 };
 
 export default TestExtendsGetServerSidePropsPage;
 
-// export const getServerSideProps = extendsGetServerSideProps(() => {
-// 	return;
-// });
+export const getServerSideProps = extendsGetServerSideProps(
+	async ({ queryClient, dehydrate }) => {
+		const id = '2';
+
+		await queryClient.prefetchQuery({
+			queryKey: testReactQueryKeys.testReactQuery({ id }),
+			queryFn: () => api.getTestReactQuery({ id }),
+		});
+
+		// const serverSideError = new ServerSideError();
+		// throw serverSideError.notFound();
+		// throw serverSideError.redirect({
+		// 	destination: '/',
+		// });
+
+		return {
+			props: {
+				dehydratedState: dehydrate(queryClient),
+			},
+		};
+	}
+);
--- src/services/next/serverSideError.ts
+++ src/services/next/serverSideError.ts
@@ -1,19 +1,57 @@
-import { GetServerSidePropsResult } from 'next';
+import { GetServerSidePropsResult, Redirect } from 'next';
 
-type ServerSideErrorResultType = Exclude<
+type ServerSidePropsResultExcludeProps = Exclude<
 	GetServerSidePropsResult<any>,
-	{ props: any | Promise<any> }
+	{ props: any }
 >;
 
-class ServerSideError {
-	private errorInfo: ServerSideErrorResultType;
+type PartialRedirect = Omit<Redirect, 'permanent'> & {
+	permanent?: boolean;
+};
 
-	constructor(props: ServerSideErrorResultType) {
-		this.errorInfo = props;
+class ServerSideError {
+	private errorInfo: {
+		serverSideResult: ServerSidePropsResultExcludeProps;
+	} | null = null;
+
+	public notFound() {
+		this.setServerSideResult({ notFound: true });
+		return this;
 	}
 
-	public getError() {
-		throw this.errorInfo;
+	/**
+	 * @description extendsGetServerSideProps 함수 내 서버사이드에서 특정 url로 redirect 할 때 호출합니다.
+	 * @param destination
+	 * * string
+	 * 	- redirect 할 url 전달
+	 * @param permanent
+	 * * true
+	 * 	- 308 상태코드
+	 * 	- 영구 리다이렉트
+	 * * false
+	 * 	- 307 상태코드
+	 * 	- 임시(temporary) 페이지로 리다이렉트
+	 */
+	public redirect(redirect: PartialRedirect) {
+		this.setServerSideResult({
+			redirect: {
+				permanent: true,
+				...redirect,
+			},
+		});
+		return this;
+	}
+
+	public getErrorInfo() {
+		return this.errorInfo;
+	}
+
+	private setServerSideResult(
+		serverSideResult: ServerSidePropsResultExcludeProps
+	) {
+		this.errorInfo = {
+			serverSideResult,
+		};
 	}
 }
 
